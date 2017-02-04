e858f47765dda674311757d7f228b92edb55d2ee
Your Name <youremail@domain.com>
2017-02-04 23:54:47 +0100

    js corrections

---
+++ script/script.js
@@ -0,0 +1,69 @@
+var tab1 = document.getElementById("tab1");
+var tab2 = document.getElementById("tab2");
+var tab3 = document.getElementById("tab3");
+var tab4 = document.getElementById("tab4");
+
+
+
+
+
+function tab11(){
+	if (tab1.style.display === "none"){
+		tab1.style.display = "block";
+		tab2.style.display = "none";
+		tab3.style.display = "none";
+		tab4.style.display = "none";
+	}
+	else{
+		tab1.style.display = "none";
+		tab2.style.display = "none";
+		tab3.style.display = "none";
+		tab4.style.display = "none";
+	}
+}
+
+function tab22(){
+	if (tab2.style.display === "none"){
+		tab2.style.display = "block";
+		tab1.style.display = "none";
+		tab3.style.display = "none";
+		tab4.style.display = "none";
+	}
+	else{
+		tab1.style.display = "none";
+		tab2.style.display = "none";
+		tab3.style.display = "none";
+		tab4.style.display = "none";
+	}
+}
+
+function tab33(){
+	if (tab3.style.display === "none"){
+		tab3.style.display = "block";
+		tab2.style.display = "none";
+		tab1.style.display = "none";
+		tab4.style.display = "none";
+	}
+	else{
+		tab1.style.display = "none";
+		tab2.style.display = "none";
+		tab3.style.display = "none";
+		tab4.style.display = "none";
+	}
+}
+
+function tab44(){
+	if (tab4.style.display === "none"){
+		tab4.style.display = "block";
+		tab2.style.display = "none";
+		tab3.style.display = "none";
+		tab1.style.display = "none";
+	}
+	else{
+		tab1.style.display = "none";
+		tab2.style.display = "none";
+		tab3.style.display = "none";
+		tab4.style.display = "none";
+	}
+}
+
